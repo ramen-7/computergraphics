17c71975a6890cda402646ea99520ec7b6852968
Shivam Taneja <76044999+ramen-7@users.noreply.github.com>
2023-02-23 10:39:24 +0530

    Update bresenhman-line.cpp

--- bresenhman-line.cpp
+++ bresenhman-line.cpp
@@ -1,74 +1,75 @@
-#include <GL/glut.h>  // include GLUT library
 #include <iostream>
-
+#include <math.h>
+#include <GL/glut.h>
 using namespace std;
-
-int x1, y1, x2, y2;
-
-void init() {
-    glClearColor(0.0, 0.0, 0.0, 1.0);  // set clear color to black
-    glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(0, 500, 0, 500);  // set up a 2D coordinate system
+GLdouble X1, Y1, X2, Y2;
+void display(void)
+{
+	int dx = abs(X1 - X2);
+	int dy = abs(Y1 - Y2);
+	int xInc = X2 > X1 ? 1 : -1;
+	int yInc = Y2 > Y1 ? 1 : -1;
+	int x = X1, y = Y1;
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POINTS);
+	glVertex2i(x, y);
+	if (dx > dy)
+	{
+		int p = 2 * dy - dx;
+		for (int i = 0; i < dx; i++)
+		{
+			if (p >= 0)
+			{
+				y += yInc;
+				p -= 2 * dx;
+			}
+			x += xInc;
+			p -= 2 * dy;
+			glVertex2d(x, y);
+		}
+	}
+	else //if dx < dy
+	{
+		int p = 2 * dx - dy;
+		for (int i = 0; i < dy; i++)
+		{
+			if (p >= 0)
+			{
+				x += xInc;
+				p -= 2 * dy;
+			}
+			y += yInc;
+			p += 2 * dy;
+			glVertex2i(x, y);
+		}
+	}
+	glEnd();
+	glFlush();
 }
 
-void plot(int x, int y) {
-    glBegin(GL_POINTS);
-    glVertex2i(x, y);
-    glEnd();
+void init()
+{
+	glClearColor(1.0, 1.0, 1.0, 0);
+	glColor3f(0.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, 500, 0, 500);
 }
 
-void bresenham(int x1, int y1, int x2, int y2) {
-    int dx = abs(x2 - x1);
-    int dy = abs(y2 - y1);
-    int incx = x2 > x1 ? 1 : -1;
-    int incy = y2 > y1 ? 1 : -1;
-    int x = x1;
-    int y = y1;
-    plot(x, y);
-    if (dx > dy) {
-        int e = 2 * dy - dx;
-        for (int i = 0; i < dx; i++) {
-            if (e >= 0) {
-                y += incy;
-                e -= 2 * dx;
-            }
-            x += incx;
-            e += 2 * dy;
-            plot(x, y);
-        }
-    } else {
-        int e = 2 * dx - dy;
-        for (int i = 0; i < dy; i++) {
-            if (e >= 0) {
-                x += incx;
-                e -= 2 * dy;
-            }
-            y += incy;
-            e += 2 * dx;
-            plot(x, y);
-        }
-    }
-}
-
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 1.0);  // set drawing color to white
-    bresenham(x1, y1, x2, y2);
-    glFlush();  // force any buffered drawing to complete
-}
-
-int main(int argc, char** argv) {
-    cout << "Enter the starting point (x1, y1): ";
-    cin >> x1 >> y1;
-    cout << "Enter the ending point (x2, y2): ";
-    cin >> x2 >> y2;
-
-    glutInit(&argc, argv);  // initialize GLUT
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);  // set up display mode
-    glutInitWindowSize(500, 500);  // set up window size
-    glutCreateWindow("Bresenham Line Algorithm");  // set up window title
-    init();  // call initialization function
-    glutDisplayFunc(display);  // call display function
-    glutMainLoop();  // enter event-processing loop
-    return 0;
+int main(int argc, char** argv)
+{
+	cout << "Enter Two Points for Draw LineBresenham:\n";
+	cout << "\nEnter Point1( X1 , Y1):";
+	cin >> X2 >> Y2;
+	cout << "\nEnter Point2( X2 , Y2):";
+	cin >> X2 >> Y2;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("LineBresenham");
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
 }
